6438ca9cdce463ee586f974239b5179a88651f1c
Aung Kaung Myat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-08-20 21:02:46 +0700

    feat: folders and files structure

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <Text style={{ color: "red" }}>Profile</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
